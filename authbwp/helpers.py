eec279cea201a38e030e663038726c7d65e49ebe
Randy Syring <randy.syring@level12.io>
2010-08-02 10:44:32 -0400

    added display_name attribute to the session user object in load_session()

--- authbwp/helpers.py
+++ authbwp/helpers.py
@@ -22,6 +22,7 @@ def after_login_url():
 def load_session_user(user_obj):
     user.id = user_obj.id
     user.login_id = user_obj.login_id
+    user.display_name = user_obj.name_or_login
     user.is_super_user = bool(user_obj.super_user)
     user.reset_required = user_obj.reset_required
     user.is_authenticated = True
