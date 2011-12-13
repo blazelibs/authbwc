4e51099c0f373c60c360ba4ee5782a0f4b952312
Randy Syring <rsyring@gmail.com>
2011-12-13 17:46:32 -0500

    better Jenkins build support

--- setup.py
+++ setup.py
@@ -1,5 +1,14 @@
 import os
 from setuptools import setup, find_packages
+from setuptools.command.develop import develop as STDevelopCmd
+
+class DevelopCmd(STDevelopCmd):
+    def run(self):
+        # add in requirements for testing only when using the develop command
+        self.distribution.install_requires.extend([
+            'WebTest',
+        ])
+        STDevelopCmd.run(self)
 
 from authbwc import VERSION
 
@@ -29,6 +38,7 @@ setup(
     packages=find_packages(exclude=['authbwc_*']),
     include_package_data=True,
     zip_safe=False,
+    cmdclass = {'develop': DevelopCmd},
     install_requires=[
         'CommonBWC>=0.1.0',
         'DataGridBWC>=0.1.0',
