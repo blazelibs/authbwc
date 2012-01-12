7beffe99c31a12e1934d69c4d522c627229c8dba
Randy Syring <randy.syring@level12.io>
2012-01-12 16:07:40 -0500

    remove next version from pypi page

--- setup.py
+++ setup.py
@@ -13,11 +13,6 @@ class DevelopCmd(STDevelopCmd):
 
 from authbwc import VERSION
 
-NEXT_VERSION = """
-Link to tip as next version (for setup.py requirements):
-http://bitbucket.org/rsyring/authbwc/get/tip.zip#egg=AuthBWC-{0}post1
-""".format(VERSION)
-
 cdir = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(cdir, 'readme.rst')).read()
 CHANGELOG = open(os.path.join(cdir, 'changelog.rst')).read()
@@ -26,7 +21,7 @@ setup(
     name='AuthBWC',
     version=VERSION,
     description="A user authentication and authorization component for the BlazeWeb framework",
-    long_description= '\n\n'.join((README, NEXT_VERSION, CHANGELOG)),
+    long_description= '\n\n'.join((README, CHANGELOG)),
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
